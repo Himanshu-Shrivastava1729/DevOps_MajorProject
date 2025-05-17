5b55ff4097f0e2594f5334f07f44b47eeb358a9d
Himanshu_Shrivastava1729 <himanshushrivastava227@gmail.com>
2025-05-18 05:27:02 +0530

    debug

--- Frontend/app.py
+++ Frontend/app.py
@@ -39,5 +39,8 @@ def index():
     return render_template("index.html", result=result)
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=5000)
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
+
